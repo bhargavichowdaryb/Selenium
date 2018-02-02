41325b749a83d826edd224c986f4c91f89e6d1c1
bhargavichowdaryb <beeminenibhargavi@gmail.com>
2018-02-02 16:52:24 +0530

    deleted_retry_listener

--- src/test/java/NewProject/NewProject/AppTest.java
+++ src/test/java/NewProject/NewProject/AppTest.java
@@ -1,7 +1,6 @@
 package NewProject.NewProject;
 
 import org.testng.annotations.Test;
-import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
